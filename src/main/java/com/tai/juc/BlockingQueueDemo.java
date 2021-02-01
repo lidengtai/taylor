275c954e177c5d0870cdb7ffcce77b5f35176152
lidengtai@168.com <lidengtai@168.com>
2021-02-01 23:20:43 +0800

    阻塞对列介绍

---
+++ src/main/java/com/tai/juc/BlockingQueueDemo.java
@@ -0,0 +1,27 @@
+package com.tai.juc;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @description: 阻塞队列
+ * LinkedBlockingQueue:有链表构成的有界阻塞队列 默认值接近无界 （Integer.MAX_VALUE)）
+ * ArrayBlockingQueue：有数组构成的有界阻塞队列
+ * SynchronousQueue：不存储元素的阻塞队列 即单个元素队列
+ * ---------------------------------
+ * PriorityBlockingQueue：支持优先级排序的无解阻塞队列
+ * DelayQueue：使用优先级队列实现的延迟无界阻塞对列
+ * LinkedTransferQueue：由链表组成的无界阻塞对列
+ * LinkedBlockingDeque：由链表组成的双向阻塞对列
+ *
+ * @author: toylor
+ * @create: 2021-02-01 22:52
+ **/
+public class BlockingQueueDemo {
+    public static void main(String[] args) {
+
+//        BlockingQueue
+//        LinkedBlockingQueue()
+    }
+}
