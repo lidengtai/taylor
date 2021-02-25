6f0a64a54809424d8e1599fa69dc84e23d529539
taylor <lidengtai@163.com>
2021-02-25 21:41:45 +0800

    jvm

--- cloud2021/cloud-provider-payment8001/src/main/java/com/taylor/springcloud/dao.java
+++
@@ -1,9 +0,0 @@
-package com.taylor.springcloud;
-
-/**
- * @author taylor
- * @Description: TODO
- * @date 2021/2/23 0:18
- */
-public class dao {
-}
--- cloud2021/cloud-provider-payment8001/src/main/java/com/taylor/springcloud/entities/PaymentEntity.java
+++
@@ -1,9 +0,0 @@
-package com.taylor.springcloud.entities;
-
-/**
- * @author taylor
- * @Description: TODO
- * @date 2021/2/23 0:12
- */
-public class PaymentEntity {
-}
